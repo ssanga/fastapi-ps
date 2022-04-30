066c044bc919691416c36ff5988b4011bbe2b0ce
Santiago Sanchez Garcia <ssanga@outlook.com>
2022-04-30 12:54:34 +0200

    relations (no funcionan bien)

---
+++ old/carsharing_with_json_file.py
@@ -0,0 +1,73 @@
+from imp import reload
+from typing import List, Optional
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+from schemas import CarInput, CarOutput, load_db, save_db, TripInput, TripOutput
+from sqlmodel import SQLModel, create_engine
+
+
+app = FastAPI(title="Car Sharing")
+db = load_db()
+
+engine = create_engine(
+    "sqlite:///carsharing.db", 
+     connect_args={"check_same_thread": False},
+     echo=True
+)
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata_create_all(engine)
+
+@app.get("/api/cars")
+def get_cars(size: Optional[str] = None, doors:Optional[int] = None) -> List:
+    result = db
+    if size:
+        result = [car for car in result if car.size == size]
+    if doors:
+        result = [car for car in result if car.doors >= doors]
+    
+    return result
+
+@app.get("/api/cars/{id}")
+def car_by_id(id : int) -> dict:
+    if result := [car for car in db if car.id == id]:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+
+@app.post("/api/cars", response_model=CarOutput)
+def add_car(car: CarInput) -> CarOutput:
+    new_car = CarOutput(size=car.size, fuel=car.fuel, doors=car.doors, transmission=car.transmission, id=len(db) + 1)
+    db.append(new_car)
+    save_db(db)
+    return new_car
+
+@app.delete("/api/cars/{id}", status_code=204)
+def remove_car(id: int) -> None:
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        db.remove(car)
+        save_db(db)
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+@app.put("/api/cars/{id}", response_model=CarOutput)
+def change_car(id: int, new_data: CarInput) -> CarOutput:
+    matches = [car for car in db if car.id == id]
+    if matches:
+        car = matches[0]
+        car.size = new_data.size
+        car.fuel = new_data.fuel
+        car.doors = new_data.doors
+        car.transmission = new_data.transmission
+        save_db(db)
+        return car
+    else:
+        raise HTTPException(status_code=404, detail=f"No car with id={id}.")
+
+if __name__ == '__main__':
+    uvicorn.run("carsharing:app", reload = True)
--- schemas.py
+++ schemas.py
@@ -1,25 +1,31 @@
-import json
-from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
 
-class TripInput(BaseModel):
+class TripInput(SQLModel):
     start: int
     end: int
     description: str
 
+
 class TripOutput(TripInput):
     id: int
 
-class CarInput(BaseModel):
+
+class Trip(TripInput, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    car_id: int = Field(foreign_key="car.id")
+    car: "Car" = Relationship(back_populates="trips")
+
+
+class CarInput(SQLModel):
     size: str
     fuel: str | None = "electric"
     doors: int
     transmission: str | None = "auto"
 
-    class Config: 
+    class Config:
         schema_extra = {
-            "example": 
-            {
+            "example": {
                 "size": "m",
                 "doors": 5,
                 "transmission": "manual",
@@ -27,18 +33,13 @@ class CarInput(BaseModel):
             }
         }
 
+
+class Car(CarInput, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    trips: list[Trip] = Relationship(back_populates="car")
+
+
 class CarOutput(CarInput):
     id: int
     trips: list[TripOutput] = []
 
-
-
-def load_db() -> list[CarOutput]:
-    """Load a list of Car objects from a JSON file"""
-    with open("cars.json", "r") as f:
-        return [CarOutput.parse_obj(car) for car in json.load(f)]
-
-def save_db(cars: list[CarInput]) -> None:
-    """Save a list of Car objects to a JSON file"""
-    with open("cars.json", "w") as f:
-        json.dump([car.dict() for car in cars], f, indent=4)
